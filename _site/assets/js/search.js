1c7256885982ec1e1c622263547700d7801cb3d8
Ellis J Alia <32417069+ellis-bio@users.noreply.github.com>
2025-07-11 18:06:41 +0000

    ud

--- _site/assets/js/search.js
+++ _site/assets/js/search.js
@@ -1,9 +1,8 @@
-// Only run on pages that have a search-input element
-if (document.getElementById('search-input')) {
+// Only run on pages that have a search-input and results element
+if (document.getElementById('search-input') && document.getElementById('results')) {
   fetch('/search.json')
     .then(res => res.json())
     .then(docs => {
-      // Build the index
       const idx = lunr(function() {
         this.field('title', { boost: 10 });
         this.field('content');
@@ -18,15 +17,15 @@ if (document.getElementById('search-input')) {
         const query = input.value.trim();
         const hits  = query ? idx.search(query) : [];
 
-        // Clear out old results
         results.innerHTML = '';
 
-        // Render hits
         hits.forEach(hit => {
           const item = docs.find(d => d.url === hit.ref);
-          const li   = document.createElement('li');
-          li.innerHTML = `<a href="${item.url}">${item.title}</a>`;
-          results.appendChild(li);
+          if (item) {
+            const li = document.createElement('li');
+            li.innerHTML = `<a href="${item.url}">${item.title}</a>`;
+            results.appendChild(li);
+          }
         });
       });
     })
--- privacy-policy.md
+++ privacy-policy.md
@@ -1,5 +1,5 @@
 ---
-layout: page
+layout: default
 permalink: /privacy-policy/
 ---
 <center>
